41de16265c2aa9c9159148d77ae5db6220f34c8b
coppercash <coderdreamer@gmail.com>
2014-11-21 19:00:11 +0800

    Clean up config

--- app/src/main/java/com/nimbusbase/nimbusbase_android_tutorial/Singleton.java
+++ app/src/main/java/com/nimbusbase/nimbusbase_android_tutorial/Singleton.java
@@ -28,36 +28,31 @@ public class Singleton {
         );
 
         public static Map<String, Object> getBaseConfigs() {
-
-            final String appName = "Nimbus iOS Tutorial";
-
-            return new HashMap<String, Object>() {
-                {
-                    put(Config.SERVERS,
-                            new ArrayList<Map<String,Object>>() {
-                                {
-                                    add(new HashMap<String, Object>() {
-                                        {
+            final String
+                    appName = "Nimbus iOS Tutorial";
+            return
+                    new HashMap<String, Object>() {{
+                        put(Config.SERVERS,
+                                new ArrayList<Map<String,Object>>() {{
+                                    add(new HashMap<String, Object>() {{
                                             put(Config.CLOUD, Config.DROPBOX);
                                             put(Config.APP_NAME, appName);
                                             put(Config.APP_ID, "x0e7vb4ls3lub5d");
                                             put(Config.APP_SECRET, "jl1xp49sumwe7tf");
                                             put(Config.AUTH_SCOPE, Config.AuthScope.APP_DATA);
-                                        }
-                                    });
-                                    add(new HashMap<String, Object>() {
-                                        {
+                                        }}
+                                    );
+                                    add(new HashMap<String, Object>() {{
                                             put(Config.CLOUD, Config.BOX);
                                             put(Config.APP_NAME, appName);
                                             put(Config.APP_ID, "eky66lnq5fnlmulhos7080u1c4a2isv2");
                                             put(Config.APP_SECRET, "6bMkjEzGKU1ghcmXbCii32ykGHJp4xZT");
                                             put(Config.AUTH_SCOPE, Config.AuthScope.ROOT);
-                                        }
-                                    });
-                                }
-                            });
-                }
-            };
+                                        }}
+                                    );
+                                }}
+                        );
+                    }};
         }
     }
 }
